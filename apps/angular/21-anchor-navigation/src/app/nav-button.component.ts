347ce48c4625300ae855635b9bc3d0a39f7a7f6c
thangshelby <n.nducthangg@gmail.com>
2025-06-25 14:33:44 +0700

    feat: chanllenge anchor routing with routerLink

--- apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
+++ apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
@@ -1,17 +1,20 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, input } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'nav-button',
   template: `
-    <a [href]="href()">
+    <a [routerLink]="[href]" [fragment]="anchor">
       <ng-content />
     </a>
   `,
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
+  imports: [RouterLink],
 })
 export class NavButtonComponent {
-  href = input('');
+  @Input() href: string = '';
+  @Input() anchor?: string = undefined;
 }
