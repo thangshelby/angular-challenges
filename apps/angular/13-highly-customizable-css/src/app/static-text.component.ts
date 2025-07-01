5a0d1efb5ea5967c4e592a1932f5af48cfac84b4
Kh01654969880oa <22520688@gm.uit.edu.vn>
2025-07-01 14:12:50 +0700

    feat: day_13 chanllenge

--- apps/angular/13-highly-customizable-css/src/app/static-text.component.ts
+++ apps/angular/13-highly-customizable-css/src/app/static-text.component.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, computed, input } from '@angular/core';
+import { Component } from '@angular/core';
 import { TextComponent } from './text.component';
 
 export type StaticTextType = 'normal' | 'warning' | 'error';
@@ -8,31 +8,23 @@ export type StaticTextType = 'normal' | 'warning' | 'error';
   selector: 'static-text',
   imports: [TextComponent],
   template: `
-    <text [font]="font()" [color]="color()">This is a static text</text>
+    <text>This is a static text</text>
   `,
+  styles: [
+    `
+      text {
+        --text-font-size: 10px;
+        --text-color: yellow;
+      }
+      :host-context(.error) text {
+        --text-font-size: 30px;
+        --text-color: red;
+      }
+      :host-context(.warning) text {
+        --text-font-size: 25px;
+        --text-color: orange;
+      }
+    `,
+  ],
 })
-export class TextStaticComponent {
-  type = input<StaticTextType>('normal');
-
-  font = computed(() => {
-    switch (this.type()) {
-      case 'error':
-        return 30;
-      case 'warning':
-        return 25;
-      default:
-        return 10;
-    }
-  });
-
-  color = computed(() => {
-    switch (this.type()) {
-      case 'error':
-        return 'red';
-      case 'warning':
-        return 'orange';
-      default:
-        return 'black';
-    }
-  });
-}
+export class TextStaticComponent {}
--- apps/angular/13-highly-customizable-css/src/app/text.component.ts
+++ apps/angular/13-highly-customizable-css/src/app/text.component.ts
@@ -1,15 +1,18 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'text',
   template: `
-    <p style="font-size: {{ font }}px; color: {{ color }}">
+    <p>
       <ng-content />
     </p>
   `,
+  styles: `
+    p {
+      font-size: var(--text-font-size, 10px);
+      color: var(--text-color, black);
+    }
+  `,
 })
-export class TextComponent {
-  font = input(10);
-  color = input('black');
-}
+export class TextComponent {}
