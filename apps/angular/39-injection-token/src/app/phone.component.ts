14c24378aff163f4f7e045d3d21a7e69699acb85
thangshelby <n.nducthangg@gmail.com>
2025-07-03 11:07:14 +0700

    feat(injectionTOken): answer:39: solution to injection token

--- apps/angular/39-injection-token/src/app/phone.component.ts
+++ apps/angular/39-injection-token/src/app/phone.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TIMER } from './data';
 import { TimerContainerComponent } from './timer-container.component';
 
 @Component({
@@ -11,5 +12,11 @@ import { TimerContainerComponent } from './timer-container.component';
     </div>
     <timer-container />
   `,
+  providers: [
+    {
+      provide: TIMER,
+      useValue: 2000,
+    },
+  ],
 })
 export default class PhoneComponent {}
